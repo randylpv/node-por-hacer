be96de8b9459c1d50ac4bf44dfacf87bf534f047
randylpv <randylpv@gmail.com>
2018-12-07 09:13:03 -0600

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,30 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+
+}
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crer un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completo de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+    });
+}
+
+const cargarDB = () => {
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+
+}
+
+const crer = (descripcion) => {
+
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let borrar = (descripcion) => {
+
+    cargarDB();
+
+    let nuevoListado = listadoPorHacer.filter(tarea => tarea.descripcion !== descripcion);
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+}
+
+module.exports = {
+    crer,
+    getListado,
+    actualizar,
+    borrar
+}
